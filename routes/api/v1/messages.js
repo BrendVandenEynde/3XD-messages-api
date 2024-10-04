144757718f8f4a96cc0b5526e9256a0f6bba5a3e
Clueless <brendvandeneynde@hotmail.be>
2024-10-04 11:59:42 +0200

    Initial commit

---
+++ routes/api/v1/messages.js
@@ -0,0 +1,14 @@
+//add routes
+const express = require('express');
+const router = express.Router();
+
+// GET /api/v1/messages
+router.get('/', (req, res, next) => {
+    res.json({ 
+        status: "success",
+        message: 'GET messages!',
+        data: { messages: [] }
+     });
+});
+
+module.exports = router;
