6d6d5958ded38a6bb1e621c1563e55f8383ed295
Clueless <brendvandeneynde@hotmail.be>
2024-10-04 12:39:02 +0200

    fix: GET messages endpoint response structure

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -4,10 +4,13 @@ const router = express.Router();
 
 // GET /api/v1/messages
 router.get('/', (req, res, next) => {
-    res.json({ 
+    res.status(200).json({ 
         status: "success",
         message: 'GET messages!',
-        data: { messages: [] }
+        data: { messages: [
+            { id: 1, title: "Hello", body: "World" },
+            { id: 2, title: "Food", body: "Bar" }
+        ] }
      });
 });
 
