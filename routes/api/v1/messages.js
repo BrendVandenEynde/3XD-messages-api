b5642706567e072ac3b7374e1402d575d446fff8
Clueless <brendvandeneynde@hotmail.be>
2024-10-04 13:36:56 +0200

    chore: reset progress

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -16,41 +16,4 @@ router.get('/', (req, res, next) => {
     });
 });
 
-// POST /api/v1/messages
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        status: "success",
-        message: 'POST messages!',
-        data: { message: { id: 3, user: "Hello", message: "World" } }
-    });
-});
-
-// GET /api/v1/messages/:id
-router.get('/:id', (req, res, next) => {
-    res.status(200).json({
-        status: "success",
-        message: 'GET message!',
-        data: { message: { id: 1, user: "Hello", message: "World" } }
-    });
-});
-
-// PUT /api/v1/messages/:id
-router.put('/:id', (req, res, next) => {
-    res.status(200).json({
-        status: "success",
-        message: 'PUT message!',
-        data: { message: { id: 1, user: "Hello", message: "World" } }
-    });
-});
-
-// DELETE /api/v1/messages/:id
-router.delete('/:id', (req, res, next) => {
-    res.status(200).json({
-        status: "success",
-        message: 'DELETE message!',
-        data: { message: { id: 1, user: "Hello", message: "World" } }
-    });
-});
-
-
 module.exports = router;
