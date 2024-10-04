bc2332b1b36efef18aa8c9e2a8d444aab1c46d6f
Clueless <brendvandeneynde@hotmail.be>
2024-10-04 12:34:48 +0200

    fix: app initialization order to resolve TypeError

--- app.js
+++ app.js
@@ -7,35 +7,37 @@ var logger = require('morgan');
 // cors
 const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-app.use(cors());
-
-var messagesRouter = require('./routes/api/v1/messages');
-
+// Create an Express application
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+// Middleware
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Routers
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var messagesRouter = require('./routes/api/v1/messages');
+
 // Use the routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/messages', messagesRouter);  // Corrected line to use messages router
+app.use('/api/v1/messages', messagesRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -44,4 +46,5 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+// Export the app module
 module.exports = app;
